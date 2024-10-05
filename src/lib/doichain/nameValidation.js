f6cdf70108661aa45b3a52625db8ca446b37e366
Nico Krause <post@nicokrause.com>
2024-10-05 14:49:34 +0500

    returning also currentNameUtxo for better data access

--- src/lib/doichain/nameValidation.js
+++ src/lib/doichain/nameValidation.js
@@ -14,7 +14,8 @@ export async function _checkName(electrumClient, currentNameAddress, _name, tota
     let utxoErrorMessage = '';
     let isNameValid = true;
     let isUTXOAddressValid = true;
-    let nameExists = false
+    let currentNameOp = true;
+    let currentNameUtxo
 
     if(!_name) {
         const nameErrorMessage = `No name provided`;
@@ -29,21 +30,19 @@ export async function _checkName(electrumClient, currentNameAddress, _name, tota
         const res = await nameShow(electrumClient, _name);
         if (res.length > 0) {
             for (let utxo of res) {
-                console.log("checkName-nameShow",utxo)
                 const scriptPubKey = utxo.scriptPubKey;
                 if (scriptPubKey && scriptPubKey.nameOp) {
                     currentNameAddress = scriptPubKey.addresses[0];
-                    nameExists=true;
+                    currentNameOp = scriptPubKey.nameOp
+                    currentNameUtxo = utxo
                 }
             }
             nameErrorMessage = `Name "${_name}" already registered under address ${currentNameAddress}`;
             isNameValid = false;
-            return { currentNameAddress, nameErrorMessage, utxoErrorMessage, isNameValid, isUTXOAddressValid, nameExists }
+            return { currentNameAddress, currentNameOp, currentNameUtxo, nameErrorMessage, utxoErrorMessage, isNameValid, isUTXOAddressValid }
         }
-        else if(totalUtxoValue <= totalAmount){ //sb.toSatoshi(
-            console.log("totalUtxoValue           :",totalUtxoValue)
-            console.log("sb.toSatoshi(totalAmount):",totalAmount)
-            utxoErrorMessage = `Funds on ${currentNameAddress} are insufficient for this name`;
+        else if(totalUtxoValue <= sb.toSatoshi(totalAmount)){ //TODO why is this here inside? Please move
+            utxoErrorMessage = `Funds on ${currentNameAddress} are insufficient for this Doichain name`;
             isUTXOAddressValid = false;
             return { currentNameAddress, nameErrorMessage, utxoErrorMessage, isNameValid, isUTXOAddressValid }
         }
